cc1fbedc9e8c74576d9c686eeaa93bd305d2166e
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-01-30 14:24:29 +0530

    implement fibonacci series using iteration

---
+++ fibSolution.js
@@ -0,0 +1,19 @@
+function fibs(noOfElements) {
+  let myFibSeq = [];
+  for (let i = 0; i < noOfElements; i++) {
+    if (myFibSeq.length < 2) {
+      myFibSeq.push(i);
+    } else {
+      myFibSeq.push(
+        myFibSeq[myFibSeq.length - 1] + myFibSeq[myFibSeq.length - 2]
+      );
+    }
+  }
+  return myFibSeq;
+}
+
+console.log(fibs(8)); // Expected output: [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fibs(0)); // Expected output: []
+console.log(fibs(1)); // Expected output: [0]
+console.log(fibs(2)); // Expected output: [0, 1]
+console.log(fibs(10)); // Expected output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
